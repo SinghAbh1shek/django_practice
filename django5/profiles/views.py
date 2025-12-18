961f99eba458cc3619fb7018967572f0160efc2b
Abhishek Singh <abhisheksingh.muj@gmail.com>
2025-12-18 12:30:10 +0530

    added logic for profile edit

--- django5/profiles/views.py
+++ django5/profiles/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
 
 def profile_page(request):
     return render(request, 'profile.html')
 
 def edit_profile(request):
+    user = request.user
+
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name', '').strip()
+        last_name = request.POST.get('last_name', '').strip()
+        phone = request.POST.get('phone', '').strip()
+        email = request.POST.get('email', '').replace(' ', '').lower()
+        username = request.POST.get('username', '').replace(' ', '').lower()
+
+        if not username or (not email and not phone):
+            print('error: please fill the required field')
+            return redirect('edit-profile')
+        
+        if not '@' in email:
+            print('error: enter valid email')
+            return redirect('edit-profile')
+        
+        if phone and (not phone.isdigit() or len(phone) != 10):
+            print('error: enter valid phone number')
+            return redirect('edit-profile')
+
+        if User.objects.exclude(id = user.id).filter(username=username).exists():
+            print('error: username already taken')
+            return redirect('edit-profile')
+
+        user.username = username
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.save()
+
+        customer = user.customer
+        customer.phone = phone
+        customer.save()
+        
+        print('Sucess: user profile edited')
+        return redirect('profile')
     return render(request, 'edit_profile.html')
