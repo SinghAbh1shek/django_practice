6b1beb28d3cbdb05004d7cdc02af455e2c463813
Abhishek Singh <abhisheksingh.muj@gmail.com>
2026-01-08 13:21:04 +0530

    add add product ui

--- django5/seller/urls.py
+++ django5/seller/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', home, name='seller_home')
+    path('', home, name='seller_home'),
+    path('add-product/', seller_add_product, name='seller_add_product'),
 ]
